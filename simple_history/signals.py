34eee5a5c24635470f96f69be4433c2ab5c35d66
Nick McCullum <nicholasmccullum@gmail.com>
2022-03-29 08:09:52 -0300

    removed providing_args

--- simple_history/signals.py
+++ simple_history/signals.py
@@ -1,23 +1,5 @@
 import django.dispatch
 
 
-pre_create_historical_record = django.dispatch.Signal(
-    providing_args=[
-        "instance",
-        "history_instance",
-        "history_date",
-        "history_user",
-        "history_change_reason",
-        "using",
-    ]
-)
-post_create_historical_record = django.dispatch.Signal(
-    providing_args=[
-        "instance",
-        "history_instance",
-        "history_date",
-        "history_user",
-        "history_change_reason",
-        "using",
-    ]
-)
+pre_create_historical_record = django.dispatch.Signal()
+post_create_historical_record = django.dispatch.Signal()
